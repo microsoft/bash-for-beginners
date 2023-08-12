82fef401f36c98f0876838c5c4f32a52208c6ee3
mesgna woldeb <mesgna.mehari8@gmail.com>
2023-08-12 13:44:44 +0100

    script variables

--- modifying-file-permissions/script.sh
+++ modifying-file-permissions/script.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-echo "Thank you for watching, Bash for Beginners!"
+echo "Thank you for watching, Bash for Beginners and i am delited !"
+echo " This is the start of a new journey " 
+Name=" This is mesgna"
+echo " This is written by $Name"
